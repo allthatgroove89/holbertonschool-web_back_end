b0662e13b209450ebe4d193d35257de449efc9be
Your Name <saul.vera787@gmail.com>
2024-07-17 13:39:11 -0400

    Refactor appendToEachArrayValue function to use consistent variable naming and remove unused variable declaration

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const value of array) {
+    const value = array.indexOf[value];
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
