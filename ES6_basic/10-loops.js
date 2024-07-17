2d3b212986833d7d28601f1fbd1e44ec77a720ff
Your Name <saul.vera787@gmail.com>
2024-07-17 13:53:43 -0400

    Refactor appendToEachArrayValue function to fix variable assignment bug

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
-    const idx = array.indexOf(value);
+    const idx = array(idx);
     array[idx] = appendString + value;
   }
 
