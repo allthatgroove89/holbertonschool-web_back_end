c5bde5b181b2bd7413eb61bf3e014245ceefa20d
Your Name <saul.vera787@gmail.com>
2024-07-19 12:02:17 -0400

    correcting syntax errors

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,21 +1,24 @@
+/* eslint-disable no-unused-vars */
 import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount
-    this._currency = currency
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
-  set amount(amount){
+
+  set amount(amount) {
     this._amount = amount;
   }
 
   get currency() {
     return this._currency;
   }
+
   set currency(currency) {
     this._currency = currency;
   }
