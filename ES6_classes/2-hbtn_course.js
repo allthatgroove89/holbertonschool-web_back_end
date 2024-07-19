b3949c694da8bc38ea01f0453555595460d2c932
Your Name <saul.vera787@gmail.com>
2024-07-18 22:35:51 -0400

    task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,32 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  // Getter for name
+  get name(){
+    this._name;
+  }
+  // Setter for name
+  set name(value){
+    this._name;
+  }
+  // Getter for length
+  get length(){
+    this._length;
+  }
+  // Setter for length
+  set length(value){
+    this._length = value;
+  }
+  // Getter for students
+  get students(){
+    this._students;
+  }
+  // Setter for students
+  set students(value){
+    this._students
+  }
+}
+export default HolbertonCourse;
