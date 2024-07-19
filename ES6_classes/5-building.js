335f0c44a86e205e812c249c948f42bf307cf739
Your Name <saul.vera787@gmail.com>
2024-07-19 13:37:51 -0400

    correcting syntax

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor.name !== 'Building') {
+    }
+  }
+
+  // getter of the sqft
+  get sqft() {
+    return this._sqft
+  }
+
+  // setter of the sqft
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+  evacuationWarningMessage() {
+    throw Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
