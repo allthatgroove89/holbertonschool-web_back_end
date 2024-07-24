ece98f020a53d3e1bf08d4df49c6447838ca2246
Your Name <saul.vera787@gmail.com>
2024-07-24 17:58:56 -0400

    chore: Update ES6 promise code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,8 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfieSignup(firstName, lastName, fileName) {
   const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return result.map((result_1) => {
-    if (result_1.status === 'fulfilled') {
+  return result.map((returnItem) => {
+    if (returnItem.status === 'fulfilled') {
       return { status: returnItem.status, value: returnItem.value || returnItem.reason };
     }
     return { status: returnItem.status, value: returnItem.reason.toString() };
