c224074f938822366a9d048e69e1f594d424b057
Your Name <saul.vera787@gmail.com>
2024-07-24 17:57:14 -0400

    correcting task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,12 @@
-import signUpUser from './4-user-promise.js'
-import uploadPhoto from './5-photo-reject.js'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfieSignup(firstName, lastName, fileName) {
   const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
   return result.map((result_1) => {
     if (result_1.status === 'fulfilled') {
-      return { status: result_1.status, value: result_1.value || result_1.reason };
-    } else {
-      return { status: result_1.status, value: result_1.reason.toString() };
+      return { status: returnItem.status, value: returnItem.value || returnItem.reason };
     }
+    return { status: returnItem.status, value: returnItem.reason.toString() };
   });
 }
