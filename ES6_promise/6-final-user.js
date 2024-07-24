b86c4847888e6d24e0c9cb41e0e80dca8e16defc
Your Name <saul.vera787@gmail.com>
2024-07-24 17:53:11 -0400

    correcting typo

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
 import signUpUser from './4-user-promise.js'
 import uploadPhoto from './5-photo-reject.js'
 
-export default function handleProfieSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(fileName, lastName),uploadPhoto(fileName)])
-  .then((result) => {
-    return result.map((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: result.status, value: result.value || result.reason };
-      } else {
-        return {status: result.status, value: result.reason.toString() };
-      }
-    });
+export default async function handleProfieSignup(firstName, lastName, fileName) {
+  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return result.map((result_1) => {
+    if (result_1.status === 'fulfilled') {
+      return { status: result_1.status, value: result_1.value || result_1.reason };
+    } else {
+      return { status: result_1.status, value: result_1.reason.toString() };
+    }
   });
 }
