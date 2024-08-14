5959b56cfc8c786f1d56a3fbb024dbfc13550c0c
Your Name <saul.vera787@gmail.com>
2024-08-14 16:32:06 -0400

    Multiple coroutines task and task 0 PEP validation corrected

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -10,7 +10,7 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds.
+    Asynchronous coroutine that waits between 0 and max_delay seconds.
 
     Args:
         max_delay (int): The maximum number of seconds to wait. Default is 10.
