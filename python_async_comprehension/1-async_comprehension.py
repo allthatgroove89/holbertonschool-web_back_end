84d85aa0b28b6d7f96d5d77619d4b15765562fc6
Your Name <saul.vera787@gmail.com>
2024-08-19 14:43:02 -0400

    Async Comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+"""
+Module demonstrating asynchronous comprehensions.
+"""
+
+
+import asyncio
+import random
+from typing import List
+from async_generator import async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers from an async generator.
+
+    Returns:
+        List[float]: List of 10 random numbers.
+    """
+    result = [number async for number in async_generator()]
+    return result
