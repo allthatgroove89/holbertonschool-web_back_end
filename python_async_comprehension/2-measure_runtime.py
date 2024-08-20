53297eaf039f5df933502a1baa12a7dd41679e83
Saul Vera <149511005+allthatgroove89@users.noreply.github.com>
2024-08-20 17:38:40 -0400

    Update 2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -19,12 +19,12 @@ async def measure_runtime() -> float:
     Returns:
         float: The total runtime in seconds.
     """
-    start_time = time.perf_counter()
+    start_time = time.time()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
     )
-    end_time = time.perf_counter()
+    end_time = time.time()
     return end_time - start_time
