6abc2def5002d2ace330c8e8e1d98d8d68748853
Saul Vera <149511005+allthatgroove89@users.noreply.github.com>
2024-08-20 17:36:32 -0400

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,7 @@ Module demonstrating asynchronous comprehensions.
 
 
 from typing import List
-from async_generator import async_generator
+from async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
