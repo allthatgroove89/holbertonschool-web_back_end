481c8f4a3c75e75db87708d40ee81252217a0705
Your Name <saul.vera787@gmail.com>
2024-08-22 10:07:20 -0400

    a function that updates a topic in python

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module for updating topics in a MongoDB collection.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Updates the topics of a document based on the name.
+    """
+    mongo_collection.update_many(
+        # search for whats going to be update/change
+        {"name": "Holberton school"},
+        # once I got what Im gonna update I "set/update" it.
+        {"$set": {"topics": topics}}
+    )
