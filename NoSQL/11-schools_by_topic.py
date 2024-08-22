d3a1e0da9b70c397cc94b513edee6524edc0e805
Your Name <saul.vera787@gmail.com>
2024-08-22 10:53:57 -0400

    a function that returns a list of topics

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for retrieving schools by topic in a MongoDB collection.
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic.
+
+    :param mongo_collection: pymongo collection object
+    :param topic: string, the topic to search for
+    :return: list of schools having the specific topic
+    """
+    return list(mongo_collection.find({"topic": topic}))
