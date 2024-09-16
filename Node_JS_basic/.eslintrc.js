d05cd052cf2e9b49753be890250750e3c49aaeaf
Your Name <saul.vera787@gmail.com>
2024-09-16 17:07:01 -0400

    updating and adding .eslintrc config file

---
+++ Node_JS_basic/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:jest/recommended',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+      'prefer-regex-literals': 'error',
+      'max-classes-per-file': 'off',
+      'no-underscore-dangle': 'off',
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
+  };
