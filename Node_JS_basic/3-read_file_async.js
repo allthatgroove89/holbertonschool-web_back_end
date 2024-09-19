0832db0dbb8018661cb2d8b8b7486c9e8d6998d6
Your Name <saul.vera787@gmail.com>
2024-09-19 18:52:24 -0400

    Http servers

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -4,35 +4,45 @@ function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        reject(Error('Cannot load the database'));
         return;
       }
+      const response = [];
+      let msg;
 
-      const lines = data
-        .toString()
-        .split('\n')
-        .filter((line) => line.trim() !== '');
-      const students = lines.slice(1).map((line) => line.split(','));
+      const content = data.toString().split('\n');
 
-      const NUMBER_OF_STUDENTS = students.length;
-      console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const NUMBER_OF_STUDENTS = students.length ? students.length - 1 : 0;
+      msg = `Number of students: ${NUMBER_OF_STUDENTS}`;
+      console.log(msg);
+
+      response.push(msg);
 
       const fields = {};
-      students.forEach((student) => {
-        const field = student[3];
-        if (!fields[field]) fields[field] = [];
-        fields[field].push(student[0]);
-      });
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
 
-      for (const [field, names] of Object.entries(fields)) {
-        console.log(
-          `Number of students in ${field}: ${names.length}. List: ${names.join(
-            ', ',
-          )}`,
-        );
+          fields[students[i][3]].push(students[i][0]);
+        }
       }
 
-      resolve();
+      delete fields.field;
+
+      for (const key of Object.keys(fields)) {
+        msg = `Number of students in ${key}: ${
+          fields[key].length
+        }. List: ${fields[key].join(', ')}`;
+
+        console.log(msg);
+
+        response.push(msg);
+      }
+      resolve(response);
     });
   });
 }
