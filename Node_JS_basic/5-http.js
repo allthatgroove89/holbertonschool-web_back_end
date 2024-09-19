0c370789d953e8affccfef5277104ca8b8da0214
Your Name <saul.vera787@gmail.com>
2024-09-19 19:27:12 -0400

    changing script

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,57 +1,36 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 
-const filePath = path.join(__dirname, 'database.csv');
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
 
-function countStudents(file) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const lines = data.trim().split('\n');
-        const fields = lines.slice(1);
-        const totalStudents = fields.length;
+const DATABASE = args[0];
 
-        const csStudents = [];
-        const sweStudents = [];
+const hostname = '127.0.0.1';
+const port = 1245;
 
-        fields.forEach((line) => {
-          const [firstName, field] = line.split(', ');
-          if (field === 'CS') {
-            csStudents.push(firstName);
-          } else if (field === 'SWE') {
-            sweStudents.push(firstName);
-          }
-        });
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-        let result = 'This is the list of our students\n';
-        result += `Number of students: ${totalStudents}\n`;
-        result += `Number of students in CS: ${
-          csStudents.length
-        }. List: ${csStudents.join(', ')}\n`;
-        result += `Number of students in SWE: ${
-          sweStudents.length
-        }. List: ${sweStudents.join(', ')}`;
+  const { url } = req;
 
-        resolve(result);
-      }
-    });
-  });
-}
-
-const app = http.createServer((req, res) => {
-  if (req.url === '/students') {
-    countStudents(filePath)
-      .then((output) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(output);
-      });
-  } else {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.statusCode = 404;
+  res.end();
 });
 
-app.listen(1245);
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
